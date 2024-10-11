333649bdded766cd716d5ac0df3ce49e7354c84d
Adarsh <adarsh@insanelyelegant.com>
2024-10-12 00:08:08 +0530

    Update index.blade.php

--- resources/views/names/index.blade.php
+++ resources/views/names/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Electron Configuration Form</title>
+    <title>WebToApp Builder</title>
     <!-- Bootstrap CSS, JS and dependencies -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
